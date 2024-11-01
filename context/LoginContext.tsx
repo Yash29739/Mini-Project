324975a30df17a53d01cf17b6685c3bfaefb7c2d
B_M_Yashwanth <bhagyapushpagiri17@gmail.com>
2024-11-01 21:53:16 +0530

    toDo List  corrections

--- context/LoginContext.tsx
+++ context/LoginContext.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { createContext, useContext, useState,useEffect, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 
 interface LoginContextProps {
   isLoggedIn: boolean;
